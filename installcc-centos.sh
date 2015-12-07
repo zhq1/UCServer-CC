7350c29db082554f91886cc68a821d7453b5da54
learnin9 <learnin9@gmail.com>
2015-12-07 15:53:56 +0800

    Update installcc-centos.sh

--- installcc-centos.sh
+++ installcc-centos.sh
@@ -310,7 +310,7 @@ function asterisk_install() {
 	#shutdown selinux
 	cd /usr/src
 	if [ ! -e ./asterisk-$asteriskver.tar.gz ]; then
-		wget http://downcc.ucserver.org:8082/Files/asterisk-$asteriskver.tar.gz
+		wget $downloadmirrors/asterisk-$asteriskver.tar.gz
 	fi
 	tar zxf asterisk-$asteriskver.tar.gz
 	if [ $? != 0 ]; then
