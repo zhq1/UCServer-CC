8078f9f510750a957ab454c5c9c0bbe4b1470af8
learnin9 <learnin9@gmail.com>
2016-10-11 17:57:20 +0800

    Update installcc-my5.7.sh

--- installcc-my5.7.sh
+++ installcc-my5.7.sh
@@ -728,6 +728,7 @@ function run() {
 	MYSQL
 	/etc/init.d/asterccd restart
 	chkconfig --del iptables
+	rm -rf /var/www/html/asterCC/app/webroot/js/fckeditor/editor/filemanager/connectors/test.html
 	echo -e "\e[32mUCServer-CC installation finish!\e[m";
 	echo -e "\e[32mPlease email to xuke@ucserver.cc to get the license!\e[m";
 }
