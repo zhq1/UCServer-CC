356cdd83431dc8b23f33c75a859ff852bdffb97c
learnin9 <learnin9@gmail.com>
2017-06-05 12:07:36 +0800

    Update installcc-my5.7.sh

--- installcc-my5.7.sh
+++ installcc-my5.7.sh
@@ -116,6 +116,7 @@ function php_install(){
 	wget $cdnmirror/php/40-soap.ini -O /etc/php.d/40-soap.ini
 	chkconfig php-fpm on
 	mkdir -p /var/lib/php/session
+	chown asterisk.asterisk /var/lib/php/session
 	echo -e "\e[32mPHP-Fpm Install OK!\e[m"
 }
 function redis_install(){
