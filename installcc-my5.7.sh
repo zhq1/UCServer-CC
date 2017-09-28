b6645d88d269a8bd300e2f5cb6b2bf5c0f92e8be
learnin9 <learnin9@gmail.com>
2017-09-28 10:30:43 +0800

    Update installcc-my5.7.sh

--- installcc-my5.7.sh
+++ installcc-my5.7.sh
@@ -682,7 +682,7 @@ function MYSQL(){
 }
 
 function CHANGE_DNS(){
-	echo "nameserver 223.5.5.5">/etc/resolv.conf
+	echo "nameserver 223.5.5.5">>/etc/resolv.conf
 	echo "nameserver 223.6.6.6">>/etc/resolv.conf
 }
 function ADD_COUNTS(){
