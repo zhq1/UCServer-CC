135943adc0e47affc3d4146e25f5dbec515fcdf7
learnin9 <learnin9@gmail.com>
2019-02-12 11:54:03 +0800

    Update installcc_for_centos6x.sh

--- installcc_for_centos6x.sh
+++ installcc_for_centos6x.sh
@@ -126,8 +126,8 @@ function redis_install(){
 	rpm -ivh $downloadmirror/redis30u-3.0.6-1.ius.el6.x86_64.rpm
 	chkconfig --level 2345 redis on
 	echo never > /sys/kernel/mm/transparent_hugepage/enabled
-	echo 511 > /proc/sys/net/core/somaxconn >>/etc/rc.local
-	sysctl -w vm.overcommit_memory=1
+	echo 511 > /proc/sys/net/core/somaxconn
+	sysctl -w vm.overcommit_memory=1  >/dev/null 2>&1
 	/etc/init.d/redis start
 	wget $downloadmirror/redis.init -O /etc/init.d/redis
 	chmod +x /etc/init.d/redis
